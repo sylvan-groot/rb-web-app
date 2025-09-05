1c484e1556d0f17a6858c79d2a3cb8bdb2afd0d8
Sylvan <sylvan.groot@hva.nl>
2025-09-05 13:48:15 +0200

    added back- and frontend framework

---
+++ web-frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/hello")
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from Backend:</h1>
+      <p>{data}</p>
+    </div>
+  );
+}
+
+export default App;
